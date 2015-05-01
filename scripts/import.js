ab605527dede7650b7d169c2d793998e1b8c96ad
Bob Ren <bob@codecademy.com>
2015-05-01 13:02:38 -0500

    Upload more captions

--- scripts/import.js
+++ scripts/import.js
@@ -13,7 +13,7 @@ require("glob").glob("../captions/second/*.json", function (er, files) {
   console.log (files)
   files.forEach(function (filename) {
     var file = fs.readFileSync(filename).toString();
-    console.log("// Part " + filename.split("Part_").split("-")[0]);
+    console.log("// Part " + filename.split("Part_")[1].split("-")[0]);
     console.log(file + ",");
   });
 });
