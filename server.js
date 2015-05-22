7126cf4d4788fdb30b93212d8f8c4665a114d18a
Bob Ren <bob@codecademy.com>
2015-05-21 20:50:04 -0500

    Add full transcription flow

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var Router = require('node-simple-router');
 var fs = require('fs');
 var router = Router();
 var zlib = require('zlib');
+var path = require('path');
+var mime = require('mime');
+var webvtt = require('./scripts/webvtt');
 
 var indexHTML = fs.readFileSync('search.html').toString();
 router.get('/', function (request, response) {
@@ -10,6 +13,37 @@ router.get('/', function (request, response) {
     'Content-Type': 'text/html'
   });
   response.end(indexHTML);
+});
+
+var uploadHTML = fs.readFileSync('upload.html').toString();
+router.get('/upload', function (request, response) {
+  response.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  response.end(uploadHTML);
+})
+
+router.post('/download', function(request, response) {
+  var transcriptions = JSON.parse(request.post.transcriptions);
+  var fileNumber = Math.round(Math.random() * 10000)
+  fs.writeFileSync("Public/Downloads/" + fileNumber + ".webvtt", webvtt(transcriptions));
+  response.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  response.end(JSON.stringify({fileNumber: fileNumber}));
+});
+
+router.get('/download/webvtt/:fileNumber', function (request, reponse) {
+  var file = "Public/Downloads/" + request.params.fileNumber + ".webvtt";
+
+  var filename = path.basename(file);
+  var mimetype = mime.lookup(file);
+
+  reponse.setHeader('Content-disposition', 'attachment; filename=' + filename);
+  reponse.setHeader('Content-type', mimetype);
+
+  var filestream = fs.createReadStream(file);
+  filestream.pipe(reponse);
 });
 
 var firstPassHTML = fs.readFileSync('index.html').toString();
@@ -21,6 +55,33 @@ router.get('/first/:videoIndex/:id', function (request, response) {
   });
   response.end(firstPassHTML);
 });
+
+router.get('/Video/:fileName', function (request, response) {
+  var file = path.resolve(__dirname + "/Video/", request.params.fileName + ".webm");
+  var range = request.headers.range;
+  var positions = range.replace(/bytes=/, "").split("-");
+  var start = parseInt(positions[0], 10);
+
+  fs.stat(file, function(err, stats) {
+    var total = stats.size;
+    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    var chunksize = (end - start) + 1;
+
+    response.writeHead(206, {
+      "Content-Range": "bytes " + start + "-" + end + "/" + total,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    });
+
+    var stream = fs.createReadStream(file, { start: start, end: end })
+      .on("open", function() {
+        stream.pipe(response);
+      }).on("error", function(err) {
+        response.end(err);
+      });
+  });
+})
 
 router.post('/first', function (request, response) {
   var stats = JSON.parse(request.post.stats);
