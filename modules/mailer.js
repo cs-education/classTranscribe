e65fe6236a831a5223823a976fc87c594faf5c44
Oliver Melvin <omelvin2@illinois.edu>
2015-11-06 13:44:20 -0600

    changed progress page to send students an email with their progress due to FERPA violation

---
+++ modules/mailer.js
@@ -0,0 +1,46 @@
+var nodemailer = require('nodemailer')
+var mailerPass = process.env.MAILER_PASS;
+
+if (!mailerPass) throw "Need a password in environmental variables!";
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'classtranscribe@gmail.com',
+    pass: mailerPass
+  }
+});
+
+function sendEmail(to, subject, body) {
+  var mailOptions = {
+    from: 'Class Transcribe Team <classtranscribe@gmail.com>',
+    to: to,
+    subject: subject, 
+    text: body
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
+}
+
+function progressEmail(netId, className, transcriptionCount) {
+  var emailSuffix = '@illinois.edu';
+  var subject = className + ' Class Transcribe Progress';
+  var body = [
+    'Hi,',
+    'You have completed ' + transcriptionCount + ' transcriptions for ' + className + '.',
+    'If you have any questions/concerns please email classtranscribe@gmail.com.',
+    'Thank you for participating!',
+    'The Class Transcribe Team'
+    ].join('\n\n');
+  sendEmail(netId + emailSuffix, subject, body);
+}
+
+module.exports = {
+    progressEmail: progressEmail
+};
+
