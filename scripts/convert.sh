a76389f44dc8c7ee1de5024b80e21e3ffeb51a69
Bob Ren <bob@codecademy.com>
2015-11-05 22:11:30 -0600

    No scale

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -3,6 +3,6 @@ for file in *m4v; do mv "$file" `echo $file | tr ' ' '_'`  ; done
 for file in *m4v; do mv "$file" `echo $file | tr -d '('`  ; done
 for file in *m4v; do mv "$file" `echo $file | tr -d ')'`  ; done
 for i in *.m4v ; do
-	ffmpeg -i "$i" -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-1:480 -threads 0 "${i/.m4v}".mp4
+	ffmpeg -i "$i" -codec:v libx264 -profile:v high -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -threads 0 "${i/.m4v}".mp4
 	ffmpeg -i "$i" -f wav -ar 22050 $(basename "${i/.m4v}").wav
 done
