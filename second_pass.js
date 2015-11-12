d602630b8f3335197ab89aed05d484ef988a526b
Evening Wang <ywang282@illinois.edu>
2015-11-11 23:38:01 -0600

    WIP - Integrate P2FA

---
+++ second_pass.js
@@ -0,0 +1,62 @@
+var spawn = require('child_process').spawn;
+var client = require('./modules/redis');
+var lock = false;
+
+function secondPass() {
+  client.keys("ClassTranscribe::First::*", function (err, keys) {
+    console.log(keys);
+    if (keys.length === 0) {
+      lock = false;
+      return;
+    }
+
+    var key = keys[0];
+    var className = key.split("::")[2];
+    console.log(className)
+
+    client.smembers(key, function (err, members) {
+      if (err) {
+        console.log(err);
+      }
+
+      member = members[0];
+      console.log(member);
+      var videoIndexNetid = member.replace(".txt", "");
+      console.log(videoIndexNetid);
+      var command = 'python';
+      var args = ["runner.py", className, videoIndexNetid];
+      console.log(args);
+      var child = spawn(command, args);
+      child.on("error", function (err) {
+        console.log("failed to execute the second process");
+        console.log(err);
+      })
+      child.stderr.on('data', process.stderr.write.bind(process.stderr));
+      child.stdout.on('data', process.stdout.write.bind(process.stdout));
+      child.on('close', function (code) {
+        if (code === 0) {
+          client.smove(key, "ClassTranscribe::Finished::" + className, member, function (err,status) {
+            if (err) {
+              console.log(err);
+            }  
+          })
+        }
+
+        lock = false;
+      })
+    })
+  })
+}
+
+setInterval(function () {
+  if (lock === false) {
+    lock = true;
+    secondPass();
+  }
+}, 1000*2)
+
+
+
+
+
+
