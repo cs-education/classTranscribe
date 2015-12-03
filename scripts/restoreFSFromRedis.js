b6285baa44693cd6ff54584a2c98ef28b732129e
Bob Ren <bob@codecademy.com>
2015-12-03 15:52:58 -0600

    Tabs => Spaces

--- scripts/restoreFSFromRedis.js
+++ scripts/restoreFSFromRedis.js
@@ -4,36 +4,36 @@ var client = require('../modules/redis');
 
 //restore transcriptions
 var restoreFS = function (partToRestore) {
-	var keyBase = 'ClassTranscribe::' + partToRestore + '::';
-	client.keys(keyBase + '*', function (err, keys) {
-		if (err) {
-			throw err;
-		}
+  var keyBase = 'ClassTranscribe::' + partToRestore + '::';
+  client.keys(keyBase + '*', function (err, keys) {
+    if (err) {
+      throw err;
+    }
 
-		keys.forEach(function(key) {
-			client.smembers(key, function (err, members) {
-				// there should only be one member
-				if (members.length > 1) {
-					console.log('more than 1')
-				}
+    keys.forEach(function(key) {
+      client.smembers(key, function (err, members) {
+        // there should only be one member
+        if (members.length > 1) {
+          console.log('more than 1')
+        }
 
-				var transcription = members[0];
-				var transcriptionPath = key.slice(keyBase.length);
+        var transcription = members[0];
+        var transcriptionPath = key.slice(keyBase.length);
 
-				var folders = transcriptionPath.split('/');
-				folders.pop();
-				folders = folders.join('/')
-				
-				mkdirp(folders, function (err) {
-					if (err) {
-						console.log(err);
-					}
-					console.log('creating ' + transcriptionPath)
-					fs.writeFileSync(transcriptionPath, transcription, {mode: 0777});
-				})
-			});
-		});
-	});
+        var folders = transcriptionPath.split('/');
+        folders.pop();
+        folders = folders.join('/')
+
+        mkdirp(folders, function (err) {
+          if (err) {
+            console.log(err);
+          }
+          console.log('creating ' + transcriptionPath)
+          fs.writeFileSync(transcriptionPath, transcription, {mode: 0777});
+        })
+      });
+    });
+  });
 }
 
 restoreFS("Transcriptions")
