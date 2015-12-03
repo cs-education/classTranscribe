899a19b1f0f091c8d19076b5fb45c1b70525b3c9
Bob Ren <bob@codecademy.com>
2015-12-03 15:06:58 -0600

    Make redis host configurable

--- modules/redis.js
+++ modules/redis.js
@@ -1,6 +1,7 @@
 var redis = require("redis");
+var redisHost = process.env.REDIS_HOST;
 var redisPass = process.env.REDIS_PASS;
 
 if (!redisPass) throw "Need a password in environmental variables!";
 
-module.exports = redis.createClient(6379, '54.148.52.89', {auth_pass: redisPass});
+module.exports = redis.createClient(6379, redisHost, {auth_pass: redisPass});
