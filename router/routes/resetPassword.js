99e461f48f90d411de02cf9a8a2e053547b0c531
Shirley Huang <shirley.huang.93@gmail.com>
2017-09-13 12:42:40 -0500

    added license to resetPassword and signup

--- router/routes/resetPassword.js
+++ router/routes/resetPassword.js
@@ -1,8 +1,13 @@
+/** Copyright 2015 Board of Trustees of University of Illinois
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 var router = express.Router();
 var fs = require('fs');
 
 var resetPasswordMustache = fs.readFileSync(mustachePath + 'resetPassword.mustache').toString();
-
 router.get('/resetPassword', function (request, response) {
     response.writeHead(200, {
         'Content-Type': 'text.html'
--- router/routes/signup.js
+++ router/routes/signup.js
@@ -1,8 +1,13 @@
+/** Copyright 2015 Board of Trustees of University of Illinois
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 var router = express.Router();
 var fs = require('fs');
 
 var signupMustache = fs.readFileSync(mustachePath + 'signup.mustache').toString();
-
 router.get('/signup', function (request, response) {
     response.writeHead(200, {
         'Content-Type': 'text.html'
