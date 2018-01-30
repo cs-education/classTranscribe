2f4173968e0309fb0dd1a4f2e7abf77e8659a315
Shirley Huang <shirley.huang.93@gmail.com>
2018-01-30 08:56:16 -0600

    added documentation for resetPassword

--- router/routes/resetPassword.js
+++ router/routes/resetPassword.js
@@ -9,6 +9,7 @@ var fs = require('fs');
 var client = require('./../../modules/redis');
 var crypto = require('crypto');
 
+// Variables that will be passed into the command line when running containers
 var nodemailer = require('nodemailer');
 var mailID = process.env.EMAIL_ID;
 var mailPass = process.env.EMAIL_PASS;
@@ -25,8 +26,10 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+// Get the mustache page that will be rendered for the resetPassword route
 var resetPasswordMustache = fs.readFileSync(mustachePath + 'resetPassword.mustache').toString();
 
+// Render the resetPassword mustache page
 router.get('/resetPassword', function (request, response) {
     response.writeHead(200, {
         'Content-Type': 'text.html'
@@ -34,22 +37,25 @@ router.get('/resetPassword', function (request, response) {
     renderWithPartial(resetPasswordMustache, request, response);
 });
 
+// Reset password and change in database after form is submitted
 router.post('/resetPassword/submit', function(request, response) {
+    // Get the current user's data to access information in database
     var email = request.body.email;
 
     // Check if email is already in the database
     client.hgetall("ClassTranscribe::Users::" + email, function(err, obj) {
+        // Display error when account does not exist in the database
         if (!obj) {
             var error = "Account does not exist";
             console.log(error);
             response.send({ message: error, html: '' });
         } else {
+            // Redirect user to new mustache web with a note to check their email
             response.send({ message: 'success', html: '../accountRecovery' })
 
+            // Generate a unique link specific to the user
             crypto.randomBytes(48, function(err, buffer) {
                 var token = buffer.toString('hex');
-                // console.log(token);
-
                 var host = request.get('host');
                 var link = "https://" + host + "/changePassword?email=" + email + "&id=" + token;
 
@@ -61,6 +67,7 @@ router.post('/resetPassword/submit', function(request, response) {
                     html: 'Hi, <br><br> We have just received a password reset request for ' + email + '. Please click this <a href=' + link + '>link</a> to reset your password. <br><br> Thanks! <br> ClassTranscribe Team'
                 };
 
+                // Add the token ID to database to check it is linked with the user
                 client.hmset("ClassTranscribe::Users::" + email, [
                     'change_password_id', token
                 ], function(err, results) {
@@ -68,9 +75,10 @@ router.post('/resetPassword/submit', function(request, response) {
                     console.log(results);
                 });
 
+                // Send the custom email to the user
                 transporter.sendMail(mailOptions, (error, response) => {
                     if (err) console.log(err)
-                    // console.log("Send mail status: " + response);
+                    console.log("Send mail status: " + response);
                 });
             });
 
