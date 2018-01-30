5fd0d8339c95f5f185e14b7f6f5949756806b4a8
Shirley Huang <shirley.huang.93@gmail.com>
2018-01-30 08:50:57 -0600

    added documentation for dashboard

--- router/routes/dashboard.js
+++ router/routes/dashboard.js
@@ -8,14 +8,17 @@ var router = express.Router();
 var fs = require('fs');
 var client = require('./../../modules/redis');
 
+// Get the mustache page that will be rendered for the dashboard route
 var dashboardMustache = fs.readFileSync(mustachePath + 'dashboard.mustache').toString();
 
+// Render the dashboard mustache page; if account is not authenticated, redirect to homepage
 router.get('/dashboard', function (request, response) {
     if (request.isAuthenticated()) {
         response.writeHead(200, {
             'Content-Type': 'text/html'
         });
 
+        // Obtain user information to display user data
         var email = request.user.email;
         var user = email.substr(0, email.indexOf('@'));
         var html = Mustache.render(dashboardMustache, { user: user });
