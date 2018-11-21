23a81235389624fc73cec8ff8b1ae3d0cb3807da
@rMaxiQp <zq2@illinois.edu>
2018-11-20 20:29:33 -0600

    allow google login

--- authentication.js
+++ authentication.js
@@ -12,6 +12,7 @@ var passwordHash = require('./node_modules/password-hash/lib/password-hash');
 const client = require('./db/db');
 const utils = require('./utils/logging');
 const log = utils.log;
+const info = utils.info;
 const perror = utils.perror;
 
 ensureAuthenticated = function(req, res, next) {
@@ -57,51 +58,10 @@ passport.use(new LocalStrategy(
           // Return the user if the login value matches the database
           return done(null, userInfo);
         }
-
-        // client.get("ClassTranscribe::UserLookupTable::" + username, function(err, usr) {
-        //     // Display error if the account does not exist
-        //     if (!usr) {
-        //         var error = "Account does not exist";
-        //         console.log(error);
-        //         return done(null, false, { message: error });
-        //     } else {
-        //         var username = usr;
-        //         // Check if the user is verified their email address
-				// client.hget("ClassTranscribe::Users::" + username, "verified", function(err, obj) {
-        //             console.log("Is the email verified? " + obj);
-        //             // Display error if email has not been verified
-				// 	if (obj != "true") {
-				// 		var error = "Email not verified";
-        //                 console.log(error);
-				// 		return done(null, false, { message: error });
-				// 	} else {
-						// Verify the inputted password is equivalent to the hashed password stored in the database
-						// client.hget("ClassTranscribe::Users::" + username, "password", function(err, obj) {
-						// 	var isCorrectPassword = passwordHash.verify(password, obj)
-            //                 console.log("Do the passwords match? " + isCorrectPassword);
-            //                 // Display error if password does not match the one stored in the database
-						// 	if (!isCorrectPassword) {
-						// 		var error = "Invalid password";
-            //                     console.log(error);
-						// 		return done(null, false, { message: error });
-						// 	} else {
-            //     // Return the user if the login value matches the database
-						// 		var suser = { firstname: usr['first_name'], lastname: usr['last_name'], email: username, verified: usr['verified'], university: usr['university'] };
-						// 		return done(null, suser);
-						// 	}
-						// });
-				// 	}
-				// });
-        //     }
-        });
+      }); /* client.getUserByEmail() */
     }
 ));
 
-
-var testInfo = {
-    university: 'test uni',
-    verifiedId: 'sample-verification-buffer',
-};
 const permission = require('./router/routes/permission');
 
 var configAuth = require('./config/auth');
@@ -119,32 +79,41 @@ passport.use(new GoogleStrategy({
         // make the code asynchronous
         // User.findOne won't fire until we have all our data back from Google
         process.nextTick(function () {
-            console.log(profile);
+          console.log(profile)
             client.getUserByGoogleId(profile.id).then(result => {
                 // Display error if the account does not exist
                 if (!result) {
 
                     // if the user isnt in our database, create a new user
                     // set all of the relevant information
-                    testInfo.google = {
-                        id: profile.id,
-                        token: token,
-                        name: profile.displayName,
-                        email: profile.emails[0].value // pull the first email
+                    var googleInfo = {
+                        googleId: profile.id,
+                        verifiedId: token,
+                        mailId: profile.emails[0].value, // pull the first email
+                        firstName: profile.name.givenName,
+                        lastName : profile.name.familyName,
+                        university : getUniversity(profile.emails[0].value)
                     };
-                    testInfo.mailId = profile.emails[0].value;
-                    testInfo.firstName = profile.name.givenName;
-                    testInfo.lastName = profile.name.familyName;
 
-
-                    client.createUser(testInfo).then(
+                    client.createUser( googleInfo ).then(
                         result => {
                             var userInfo = result;
                             permission.addUser(userInfo.mailId);
-                            return client.verifyUser('sample-verification-buffer', 'testing@testdomabbccc.edu');
+                            client.verifyUser(userInfo.verifiedId, userInfo.mailId).then(
+                              result => {
+
+                                return done(null, result);
+                              })
+                              .catch(err => {
+                                perror(err);
+                                /* error occur */
+                                return done(null,null);
+                              });
                         })
-                        .catch(err => { perror(err); });
-                    return done(null, testInfo);
+                        .catch(err => {
+                          perror(err);
+                          return done(null, result);
+                      });
                 } else {
                     var userInfo = result;
                     // Return the user if the login value matches the database
@@ -180,16 +149,18 @@ function findUser(id,cb){
     }
     return cb(false, result);
   }).catch(err => cb(err, null));
-    // client.hgetall("ClassTranscribe::Users::" + id, function(err, usr) {
-    //     if(err){
-    //         return cb(err,null)
-    //     }
-    //     if (!usr) {
-    //         cb(false,null)
-    //     }
-    //     else {
-    //         var user = { firstname: usr['first_name'], lastname: usr['last_name'], email: id, verified:usr['verified'], university:usr['university'] };
-    //         cb(false,user)
-    //     }
-    // });
+}
+
+// same function in router/routes/signup.js
+// Look up and return the university name from the email domain name
+// Data file comes from https://github.com/Hipo/university-domains-list
+function getUniversity(email) {
+    var domain = email.split('@')[1];
+    var data = JSON.parse(fs.readFileSync('./utils/world_universities_and_domains.json'));
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].domains[0] == domain) {
+            return data[i].name;
+        }
+    }
+    return "Unknown University";
 }
