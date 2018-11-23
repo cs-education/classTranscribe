e3508bc6d458827460b2b21fdffb6ad93068d2d8
xyrng <xyr1217@gmail.com>
2018-11-23 01:19:16 -0600

    get playlist from db

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ app.use(passport.session());
 app.set('view engine', 'ejs');
 app.use('/node_modules', express.static(path.join(__dirname,'/node_modules')));
 app.use('/vtt', express.static(path.join(__dirname,'/vtt')));
+app.use('/data', express.static(path.join(__dirname,'/../data')))
 
 /* I wasn't sure where to put these variables (that are used in various files */
 mustachePath = 'templates/';
