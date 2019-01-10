43f2464d627900d9c6f6a505094dee1d192f4de9
Chirantan Mahipal <chirantan31@gmail.com>
2019-01-11 01:08:48 +0530

    bug

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ var argv = require('minimist')(process.argv.slice(2));
 var env = argv["e"] || 'production';
 
 var callbackURL;
-if (env === dev) {
+if (env === "dev") {
     callbackURL = "https://127.0.0.1/auth/google/callback";
 } else {
     callbackURL = "https://www.classtranscribe.com/auth/google/callback"
