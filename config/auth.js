4d4b508c9fd7ef0258daa0498b9540bf6b826847
Chirantan Mahipal <chirantan31@gmail.com>
2019-01-15 23:17:54 -0600

    dev hack

--- config/auth.js
+++ config/auth.js
@@ -1,6 +1,15 @@
+var argv = require('minimist')(process.argv.slice(2));
+var env = argv["e"] || 'production';
+
+var callbackURL;
+if (env === "dev") {
+    callbackURL = "https://127.0.0.1/auth/google/callback";
+} else {
+    callbackURL = "https://www.classtranscribe.com/auth/google/callback"
+}
+
 module.exports = {
     'googleAuth': {
-        'callbackURL': 'https://classtranscribe.ncsa.illinois.edu/auth/google/callback'
+        'callbackURL': callbackURL
     }
-
 };
