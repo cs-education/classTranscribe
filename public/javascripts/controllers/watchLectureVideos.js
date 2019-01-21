6033c09f7adf04c529c9197899d56eac1d45bdf8
Chirantan Mahipal <chirantan31@gmail.com>
2019-01-21 13:12:55 -0600

    Share first commit

--- public/javascripts/controllers/watchLectureVideos.js
+++ public/javascripts/controllers/watchLectureVideos.js
@@ -11,15 +11,33 @@ var fullCourseSearch = false;
 var autoScroll = true;
 var live_transcriptions_div = $('#live_transcriptions');
 
-function navigateToVideo(startTime, video) {
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+function navigateToVideo(video, startTime) {
+    console.log(startTime, video);
+    if (typeof startTime === 'string') {
+        if (startTime.indexOf('#') > -1) {
+            startTime = parseInt(startTime.substring(0, startTime.indexOf('#')))
+        } else {
+            startTime = parseInt(startTime);
+        }        
+    }
     updateCurrentVideoTranscriptions(player.currentSrc());
     $('#search').val('');
     var videoId = player.playlist.indexOf(video);
     player.playlist.currentItem(videoId);
-    player.ready(function () {
-        player.play();
-        player.currentTime(startTime / 1000);
-    });
+    player.play();
+    player.currentTime(Math.floor(startTime / 1000));
 }
 
 function msToTime(s) {
@@ -61,6 +79,48 @@ function updateCurrentVideoTranscriptions(video) {
     }
 }
 
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+        if (successful) {
+            alert("Copied link to Clipboard!");
+        }        
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+    console.log(window.location.href);
+    console.log(window.location.hostname);
+    console.log(window.location.href);
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+        alert("Copied link to Clipboard!");
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
+
+function generateShareLink(video, startTime) {
+    var shareLink = "https://" + window.location.hostname + "/watchLectureVideos/" + courseOfferingId + "?video=" + video + "&startTime=" + startTime;
+    copyTextToClipboard(shareLink);
+}
+
 function addAllTranscriptionsToList() {
     var currentList = jslinqData.toList();
     // Output it
@@ -74,8 +134,9 @@ function addAllTranscriptionsToList() {
             if (currentList[item].part === '') {
                 continue;
             }
-            var searchitem = '<button type="button" class="list-group-item" style="display:none;" onclick=navigateToVideo(' +
-                currentList[item].start + ",'" + currentList[item].video + "') id=" + currentList[item].id + ">" + msToTime(currentList[item].start) + "-->" + currentList[item].part + "</button>";
+            var searchitem = "<div class='list-group-item transcription-item' style='display:none;' id='" + currentList[item].id + "'>" + msToTime(currentList[item].start) +
+                "&emsp;" + "<a href='#' onclick=generateShareLink('" + currentList[item].video + "'," + currentList[item].start + ")> Share </a>" +
+                "&emsp;" + "<a onclick=navigateToVideo('" +currentList[item].video + "'," + currentList[item].start + ") >" + currentList[item].part + "</a> </div>";
             live_transcriptions_div.append(searchitem);
         }
         live_transcriptions_div.show();
@@ -116,6 +177,16 @@ function scrollToListItem(listItemId) {
         jslinqData = jslinq(data);
         addAllTranscriptionsToList();
         updateCurrentVideoTranscriptions(player.currentSrc());
+
+        var queryParams = getUrlVars();
+        if (queryParams.hasOwnProperty('video')) {
+            if (queryParams.hasOwnProperty('startTime')) {
+                navigateToVideo(queryParams['video'], queryParams['startTime']);
+            } else {
+                navigateToVideo(queryParams['video'], 0);
+            }
+        }
+        
 
         function update_search_results() {
             // Get query
