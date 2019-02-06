516b4ee996f682b10a1a63f3307728f547be0821
Chirantan Mahipal <chirantan31@gmail.com>
2019-02-06 15:05:58 -0600

    Updation table

---
+++ models/updation_jobs.js
@@ -0,0 +1,18 @@
+const uuid = require('uuid/v4');
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var UpdationJobs = sequelize.define('UpdationJobs', {
+        id: { type: DataTypes.UUID, primaryKey: true, defaultValue: uuid() },
+        startDate: DataTypes.DATEONLY,
+        endDate: DataTypes.DATEONLY,
+        sourceType: DataTypes.TINYINT,
+        url: DataTypes.TEXT,
+        params: DataTypes.TEXT
+    });
+
+    UpdationJobs.associate = function (models) {
+        models.UpdationJobs.belongsTo(models.CourseOffering, { foreignKey: 'courseOfferingId' });
+    };
+
+    return UpdationJobs;
+};
