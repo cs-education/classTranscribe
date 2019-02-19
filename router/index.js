3458b7517b720859083995e77a2f7c22ebcb16c2
Lawrence Angrave <angrave@illinois.edu>
2019-02-18 19:12:44 -0600

    cleanup

--- router/index.js
+++ router/index.js
@@ -12,33 +12,20 @@ module.exports = function(app) {
 */
     const router = express.Router();
 
-    // app.use(require('./routes/api'));
     app.use(require('./routes/base'));
-    app.use(require('./routes/admin'));
-    // app.use(require('./routes/utils'));
-    app.use(require('./routes/signup'));
     app.use(require('./routes/login'));
     app.use(require('./routes/logout'));
-    app.use(require('./routes/resetPassword'));
-    app.use(require('./routes/changePassword'));
-    app.use(require('./routes/settings'));
-    app.use(require('./routes/dashboard'));
-    app.use(require('./routes/accountRecovery'));
-    app.use(require('./routes/activated'));
-    app.use(require('./routes/first'));
-    app.use(require('./routes/progress'));
-    app.use(require('./routes/viewProgress'));
     app.use(require('./routes/download'));
-    app.use(require('./routes/queue'));
-    app.use(require('./routes/second'));
     app.use(require('./routes/search'));
     app.use(require('./routes/viewer'));
-    app.use(require('./routes/captions'));
-    app.use(require('./routes/manage'));
+
     app.use(require('./routes/watchLectureVideos'));
     app.use(require('./routes/courses'));
     app.use(require('./routes/scraper'));
 }
+
+
+
 
 //const authenticatedPartial = fs.readFileSync(mustachePath + 'authenticated.mustache').toString();
 //const notAuthenticatedPartial = fs.readFileSync(mustachePath + 'notAuthenticated.mustache').toString();
