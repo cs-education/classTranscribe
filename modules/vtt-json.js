dca46ee625d0756a098c789eb9a1b914d62edfe8
William Wang <williamwong0409@gmail.com>
2019-03-05 12:14:27 -0600

    remove new Promise

--- modules/vtt-json.js
+++ modules/vtt-json.js
@@ -1,44 +1,42 @@
 function convertVttToJson(vttString) {
-  return new Promise((resolve, reject) => {
-    let current = {}
-    let sections = []
-    let subtitleIsNext = false;
+  let current = {}
+  let sections = []
+  let subtitleIsNext = false;
 
-    let line_pivot = vttString.indexOf('-->');
-    while (line_pivot !== -1) {
-      let line_start = vttString.lastIndexOf('\n', line_pivot - 1);
-      let line_end = vttString.indexOf('\n', line_pivot + 3);
-      subtitleIsNext = true;
+  let line_pivot = vttString.indexOf('-->');
+  while (line_pivot !== -1) {
+    let line_start = vttString.lastIndexOf('\n', line_pivot - 1);
+    let line_end = vttString.indexOf('\n', line_pivot + 3);
+    subtitleIsNext = true;
 
-      let left_half = vttString.substring(line_start, line_pivot).trimRight();
-      let first_space_in_left_half_idx = left_half.indexOf(' ');
+    let left_half = vttString.substring(line_start, line_pivot).trimRight();
+    let first_space_in_left_half_idx = left_half.indexOf(' ');
 
-      let right_half = vttString.substring(line_pivot + 3, line_end).trimLeft();
-      let first_space_in_right_half_idx = right_half.indexOf(' ');
+    let right_half = vttString.substring(line_pivot + 3, line_end).trimLeft();
+    let first_space_in_right_half_idx = right_half.indexOf(' ');
 
-      current = {
-        //Avoid a copy if no space is found.
-        start: timeString2ms(first_space_in_left_half_idx < 0 ? left_half : left_half.substring(first_space_in_left_half_idx + 1)),
-        end: timeString2ms(first_space_in_right_half_idx < 0 ? right_half : right_half.substring(0, first_space_in_right_half_idx)),
-        subtitles: []
-      }
-      sections.push(current);
+    current = {
+      //Avoid a copy if no space is found.
+      start: timeString2ms(first_space_in_left_half_idx < 0 ? left_half : left_half.substring(first_space_in_left_half_idx + 1)),
+      end: timeString2ms(first_space_in_right_half_idx < 0 ? right_half : right_half.substring(0, first_space_in_right_half_idx)),
+      subtitles: []
+    }
+    sections.push(current);
 
+    line_start = line_end + 1;
+    line_end = vttString.indexOf('\n', line_start);
+    while (line_end > line_start + 1) {
+      current.subtitles.push(vttString.substring(line_start, line_end));
       line_start = line_end + 1;
       line_end = vttString.indexOf('\n', line_start);
-      while (line_end > line_start + 1) {
-        current.subtitles.push(vttString.substring(line_start, line_end));
-        line_start = line_end + 1;
-        line_end = vttString.indexOf('\n', line_start);
-      }
-      if (line_end === -1) {
-        break;
-      }
-      line_pivot = vttString.indexOf('-->', line_end + 1);
     }
+    if (line_end === -1) {
+      break;
+    }
+    line_pivot = vttString.indexOf('-->', line_end + 1);
+  }
 
-    resolve(sections);
-  })
+  return sections;
 }
 
 // helpers
