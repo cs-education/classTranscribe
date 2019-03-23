e2833a8056b7fcdbf7023696b3724f4cadb56431
rMaxiQp <zq2@illinois.edu>
2019-03-22 19:30:13 -0500

    finalize update ..

--- models/courseOffering.js
+++ models/courseOffering.js
@@ -3,16 +3,12 @@ const uuid = require('uuid/v4');
 
 module.exports = (sequelize, DataTypes) => {
     var CourseOffering = sequelize.define('CourseOffering', {
-<<<<<<< HEAD
-      id: { type: DataTypes.UUID, primaryKey: true },
-=======
       id: { type: DataTypes.UUID, primaryKey: true, defaultValue: uuid() },
       // 0 - Public - Video playback is available to everyone
       // 1 - Private - Only enrolled and manually added users
       // 2 - Signed In - Usage is tracked by user id
       // 3 - University Only - Authenticated and at the same institution
       role: { type : DataTypes.TINYINT },
->>>>>>> 976d7acc55896907bb40650c81c143a96f9d50a0
     });
 
     CourseOffering.associate = function(models) {
