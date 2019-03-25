7ff21f05181262e1ab9ba153efff597c7341a876
rMaxiQp <zq2@illinois.edu>
2019-03-25 01:05:46 -0500

    somewhat tested

--- modules/permission.js
+++ modules/permission.js
@@ -9,49 +9,52 @@ const User = models.User;
 const UserOffering = models.UserOffering;
 
 async function updateCoursePermission(userId, courseOfferingId, roleName) {
-
-  return Role.findOrCreate({
-    where : {roleName : roleName},
-    defaults : {id : uuid()}
-  }).then(result => {
-
-    var roleInfo = result[0].dataValues;
-
-    return UserOffering.findOrCreate({ 
-      where : {userId : userId, courseOfferingId : courseOfferingId}, 
-      defaults : {id : uuid(), roleId : roleInfo.id}
-    }).then((_, create) => {
+  try {
+      var role = await Role.findOrCreate({
+        where: { roleName: roleName },
+        defaults: { id: uuid() }
+      });
       
-      if (create === true) { return true; }
+      var roleInfo = role[0].dataValues;
+      var userOfferingResult = await UserOffering.findOrCreate({
+        where: { userId: userId, courseOfferingId: courseOfferingId },
+        defaults: { roleId: roleInfo.id }});
+      
+      if (userOfferingResult[1] === true) { 
+        return true; 
+      } else {
+        
+        var updateResult = await UserOffering.update(
+          { roleId: roleInfo.id },
+          {
+            where: {
+              userId: userId,
+              courseOfferingId: courseOfferingId
+            }
+          });
 
-      return UserOffering.update(
-        { roleId : roleInfo.id },
-        { where : {
-          userId :userId,
-          courseOfferingId : courseOfferingId
-        }}).then(result => result);
-
-    }).catch(err => {
+        return null !== updateResult;
+      }
+    } catch (err) {
       console.log(err);
       return false;
-    })
-  })
+    }
 }
 
 async function addCoursePermission(userId, courseOfferingId, permission) {
   
   switch (permission) {
-    case 'View': return updateCoursePermission(userId, courseOfferingId, 'Student');
-    case 'Edit': return updateCoursePermission(userId, courseOfferingId, 'Instructor');
-    case 'Delete': return updateCoursePermission(userId, courseOfferingId, 'Administrator');
+    case 'View': return await updateCoursePermission(userId, courseOfferingId, 'Student');
+    case 'Edit': return await updateCoursePermission(userId, courseOfferingId, 'Instructor');
+    case 'Delete': return await updateCoursePermission(userId, courseOfferingId, 'Administrator');
     default: return false; 
   }
 }
 
 async function removeCoursePermission(userId, courseOfferingId) {
   switch (permission) {
-    case 'Delete': return updateCoursePermission(userId, courseOfferingId, 'Instructor');
-    case 'Edit': return updateCoursePermission(userId, courseOfferingId, 'Student');
+    case 'Delete': return await updateCoursePermission(userId, courseOfferingId, 'Instructor');
+    case 'Edit': return await updateCoursePermission(userId, courseOfferingId, 'Student');
     case 'View': return await UserOffering.destroy( {where : {userId : userId, courseOfferingId : courseOfferingId}});
     default: return false;
   }
@@ -60,7 +63,10 @@ async function removeCoursePermission(userId, courseOfferingId) {
 async function isWatchingUniversityAllowed(userData, courseData) {
   if (userData === null || courseData === null) { return false; }
 
-  var courseUniversity = await Offering.findOne({where : {id : courseData.OfferingId}});
+  var courseUniversity = await Offering.findOne({where : {id : courseData.offeringId}});
+
+  if (courseUniversity === null) return false;
+
   courseUniversity = courseUniversity.dataValues;
 
   return userData.universityId === courseUniversity.universityId;
@@ -71,118 +77,111 @@ async function isWatchingPrivateAllowed(userData, courseData) {
 
   // Since every valid role has [View] access, we only need to check if 
   // there is a corresponding information inside of table
-  return UserOffering.findOne({ where: { userId : userData.id}}).then(
-    result => result !== null
-  );  
+  var offering = await UserOffering.findOne({ where: { userId : userData.id}});
+
+  return offering !== null;
 }
 
 async function isWatchingAllowed(userId , courseOfferingId) {
+  try {
+    var courseInfo = await CourseOffering.findOne({ where : {id : courseOfferingId}});
+    var userInfo = await User.findOne({ where : {id : userId}});
 
-  var findCourseInfo = CourseOffering.findOne({ where : {id : courseOfferingId}});
-  var findUserInfo = User.findOne({ where : {id : userId}});
-
-  return Promise.all([findCourseInfo, findUserInfo]).then(values => {
-    var data = values.map(value => value === null ? null : value.dataValues);
-    var courseData = data[0];
-    var userData = data[1];
-
-    switch(courseData.role) {
+    var courseData = courseInfo === null ? null : courseInfo.dataValues;
+    var userData = userInfo === null ? null : userInfo.dataValues;
+    
+    switch (courseData.role) {
       /* Public Courses */
       case 0: return true;
       
       /* Private Courses */
-      case 1: return isWatchingPrivateAllowed(userData, courseData).then(result => result);
+      case 1: return await isWatchingPrivateAllowed(userData, courseData).then(result => result);
       
       /* Signed In Courses */
       case 2: return userData !== null;
-
+      
       /* University Only */
-      case 3: return isWatchingUniversityAllowed(userData, courseData).then(result => result);
-
+      case 3: return await isWatchingUniversityAllowed(userData, courseData).then(result => result);
+      
       /* Don't know what it is */
       default: return false;
     }
-  })
+  } catch(err) {
+    console.log(err);
+    return false;
+  }
 }
 
 async function isManagingAllowed(userId, courseOfferingId) {
-  var findCourse = UserOffering.findOne({ 
-    where: { 
-      userId: userId, 
-      courseOfferingId: courseOfferingId
-    }});
-  
-  var findRole = Role.findOne({ where : {roleName : 'Student'}});
+  try {
 
-  return Promise.all([findCourse, findRole]).then(values => {
-    var data = values.map(value => value === null ? null : value.dataValues);
-    var courseInfo = data[0];
-    var roleInfo = data[1];
-    
-    if (courseInfo === null) { return false; }
-
-    /* Only Student has no permission of [Edit] */
-    return courseInfo.roleId !== roleInfo.id;
-  })
+    var courseInfo = await UserOffering.findOne({ 
+      where: { 
+        userId: userId, 
+        courseOfferingId: courseOfferingId
+      }});
+      
+      var roleInfo = await Role.findOrCreate({ where : {roleName : 'Student'}, defaults: {id : uuid()}});
+      
+      var courseData = courseInfo === null ? null : courseInfo.dataValues;
+      var roleData = roleInfo === null ? null : roleInfo[0].dataValues;
+      
+      if (courseData === null) { return false; }
+      
+      return courseData.roleId !== roleData.id;
+    }
+  catch(err) {
+    console.log(err);
+    return false;
+  }
 }
 
 async function allWatchableCourses(userId) {
-  var publicCourses = CourseOffering.findAll({where : {role : 0}});
-  var privateCourses = UserOffering.findAll({where : {userId : userId}});
-  var findUserInfo = User.findOne({where : {id : userId}});
-  var signInCourses = CourseOffering.findAll( { where : {role : 2}});
-  var univeristyCourses = CourseOffering.findAll({ where : {role : 3}});
+  var publicCoursesInfo = await CourseOffering.findAll({where : {role : 0}});
+  var privateCoursesInfo = await UserOffering.findAll({where : {userId : userId}});
+  var userInfo = await User.findOne({where : {id : userId}});
+  var signInCoursesInfo = await CourseOffering.findAll( { where : {role : 2}});
+  var univeristyCoursesInfo = await CourseOffering.findAll({ where : {role : 3}});
 
-  return Promise.all([publicCourses, privateCourses, findUserInfo, signInCourses, univeristyCourses]).then(values => {
-    var publicCoursesData = values[0];
-    var privateCoursesData = values[1];
-    var userInfo = values[2];
-    var signedInCoursesData = values[3];
+  // Add Public
+  var courses = publicCoursesInfo === null ? [] : publicCoursesInfo.map(course => course.dataValues.id);
 
-    // Add Public
-    var courses = publicCoursesData === null ? [] : publicCoursesData.map(course => course.dataValues.id);
+  // Add Private
+  courses.concat(privateCoursesInfo === null ? [] : privateCoursesInfo.map(course => course.dataValues.courseOfferingId ));
 
-    // Add Private
-    courses.concat( privateCoursesData === null ? [] : privateCoursesData.map(course => course.dataValues.courseOfferingId) );
-    
-    if (userInfo === null) { return courses; }
+  if (userInfo === null) { return courses; }
 
-    // Add Sign In
-    courses.concat(signedInCoursesData === null ? [] : signedInCoursesData.map(course => course.dataValues.id));
+  // Add Sign In
+  courses.concat(signInCoursesInfo === null ? [] : signInCoursesInfo.map(course => course.dataValues.id));
 
-    if (univeristyCoursesData === null) { return courses; } 
+  if (univeristyCoursesInfo === null) { return courses; } 
 
-    var universityCourses = univeristyCoursesData.map(course => course.dataValues);
-    
-    return Offering.findAll({where : {universityId : userInfo.univeristyId }}).then(result => {
-      if (result === null) { return courses; }
-      
-      result = result.map(course => course.dataValues.offeringId);    
-      
-      // Add University Only
-      courses.concat( universityCourses.filter(course => result.indexOf(course.offeringId) > 0) );
+  var univeristyCoursesData = univeristyCoursesInfo.map(course => course.dataValues);
 
-      return courses;
-    })
-  })
+  var coursesInfo = await Offering.findAll({ where: { universityId: userInfo.univeristyId } });
 
+  if (coursesInfo === null) { return courses; }
+
+  coursesData = coursesInfo.map(course => course.dataValues.offeringId);
+
+  // Add University
+  courses.concat(univeristyCoursesData.filter(course => coursesData.indexOf(course.offeringId) > 0));
+
+  return courses;
 }
 
 async function allManageableCourses(userId) {
-  var userCourses = UserOffering.findAll({ where : {userId : userId}});
-  var roleInfo = Role.findOne({ where : {roleName : 'Student'}});
+  var userCoursesInfo = await UserOffering.findAll({ where : {userId : userId}});
 
-  return Promise.all([userCourses, roleInfo]).then(values => {
-    var rawCourseData = values[0];
-    var roleData = values[1];
-    
-    if (rawCourseData === null) { return []; }
+  if (userCoursesInfo === null) { return []; }
+  
+  var roleInfo = await Role.findOne({ where : {roleName : 'Student'}});
+  var roleData = roleInfo.dataValues;
 
-    var courseData = rawCourseData.map(data => data.dataValues);
-    var managableCourses = courseData.filter(course => course.roleId !== roleData.id);
+  var userCoursesData = userCoursesInfo.map(data => data.dataValues);
+  var managableCourses = userCoursesData.filter(course => course.roleId !== roleData.id);
 
-    return managableCourses.map(course => course.id);
-  })
+  return managableCourses.map(course => course.id);
 }
 
 module.exports = {
